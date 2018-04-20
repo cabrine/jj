f98f42e895739d7d069ce5f4173714bb607c42e9
Cabrine <cabrinenakanwagi@gmail.com>
2018-04-20 04:21:27 -0800

    Initial commit

---
+++ app/src/main/java/com/example/cabp/susanhtml/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.cabp.susanhtml;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        web=(WebView)findViewById(R.id.web);
+        web.loadUrl("file:///android_asset/index.html");
+    }
+}
